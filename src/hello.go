a66dda6346d0a9398bc5c9494f5398202aaff47c
Pascal Guilcher <guilcher.p@sfeir.com>
2014-01-07 16:52:49 +0100

    add user service

--- src/hello.go
+++ src/hello.go
@@ -3,6 +3,9 @@ package src
 import (
 	"fmt"
 	"net/http"
+
+	"appengine"
+	"appengine/user"
 )
 
 func init() {
@@ -10,6 +13,17 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, everybody!")
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello, %v!", u)
 }
-
